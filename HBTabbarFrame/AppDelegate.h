abe3c358753fba5c41047ecca2dfb5de0efe19c7
pengbin <pengbin0708@163.com>
2017-11-22 15:49:54 +0800

    Initial Commit

---
+++ HBTabbarFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HBTabbarFrame
+//
+//  Created by mass on 2017/11/22.
+//  Copyright © 2017年 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HBTabbarFrame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBTabbarFrame
+//
+//  Created by mass on 2017/11/22.
+//  Copyright © 2017年 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
